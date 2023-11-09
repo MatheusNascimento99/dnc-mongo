90c5de779ee813a2b0ce046dc9dfeecebaa716a4
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2023-11-09 12:16:15 -0300

    finalização das rotas

--- index.js
+++ index.js
@@ -17,7 +17,17 @@ app.post('/familias', async(req, res) => {
 
 
 app.get('/familias', async(req, res) => {
-    const membros = await familiaModel.find({}) // comando mongodb
+
+    if (req.query.idade){
+        const membros = await familiaModel.find({}).gt('idade', req.query.idade) /* comando mongodb, a onde nessa partte <('idade', req.query.idade) >, ele passa na URL o valor da idade que 
+        deseja ou superior devido ao gt do mongo, e ainda posso passar um filtro dentro do find como nome por exemplo, para 
+        afuniliar ainda mais a busca, lt funciona como gt, mas é para baixo , buscando números menos do que o informado na url.*/
+
+        return res.status(200).json({
+            data:  membros
+        })
+    }
+    
 
     //const membrosMaiorDezAnos = membros.filter(item => item.idade >= 30) // comando em JS para filtro da informação que se queira do BD.
 
@@ -25,6 +35,7 @@ app.get('/familias', async(req, res) => {
 
     // const nomeTeste1 = membros.filter(item => item.nome. include("s")) erro ao usar .includes, verificar
 
+    const membros = await familiaModel.find({})
     return res.status(200).json({
         data:  membros
     })
@@ -33,6 +44,7 @@ app.get('/familias', async(req, res) => {
 
 
 app.get('/familias/:id', async(req, res) => {
+    try {
     const membro = await familiaModel.findOne({_id: req.params.id})  /* com findOne ele retorna um único objeto, melhor para tratar, com find apenas, ele retornará 
                                                                      um array e possivelmente terá vários objetos dentro, mais difícil de tratar*/
 
@@ -41,6 +53,13 @@ app.get('/familias/:id', async(req, res) => {
     return res.status(200).json({
         data: membroid
     })
+    }
+    catch (error) {
+        return res.status(400).json({
+            data: {},
+            message: 'Não foi possível localizar esse ID'
+        })
+    }
 })
 
 
